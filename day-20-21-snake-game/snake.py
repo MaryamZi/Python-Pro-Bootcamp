11557cf230b30d077b6e820665782831d67866ea
MaryamZi <maryamziyadm@gmail.com>
2024-01-27 10:33:30 +0530

    Add day 20 and 21 - snake game

---
+++ day-20-21-snake-game/snake.py
@@ -0,0 +1,60 @@
+from turtle import Turtle
+
+UP_HEADING = 90
+DOWN_HEADING = 270
+LEFT_HEADING = 180
+RIGHT_HEADING = 0
+
+UP = "Up"
+DOWN = "Down"
+LEFT = "Left"
+RIGHT = "Right"
+
+MOVE_DISTANCE = 20
+
+
+class Snake:
+
+    def __init__(self):
+        self.snake_segments = []
+        for x in [0, -20, -40]:
+            self.add_segment((x, 0))
+        self.head = self.snake_segments[0]
+
+    def move(self):
+        for segment_index in range(len(self.snake_segments) - 1, 0, -1):
+            preceding_segment = self.snake_segments[segment_index - 1]
+            self.snake_segments[segment_index].goto(preceding_segment.xcor(), preceding_segment.ycor())
+
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN_HEADING:
+            self.head.setheading(UP_HEADING)
+        self.move()
+
+    def down(self):
+        if self.head.heading() != UP_HEADING:
+            self.head.setheading(DOWN_HEADING)
+        self.move()
+
+    def left(self):
+        if self.head.heading() != RIGHT_HEADING:
+            self.head.setheading(LEFT_HEADING)
+        self.move()
+
+    def right(self):
+        if self.head.heading() != LEFT_HEADING:
+            self.head.setheading(RIGHT_HEADING)
+        self.move()
+
+    def add_segment(self, position):
+        snake_segment = Turtle("square")
+        snake_segment.color("white")
+        snake_segment.penup()
+        snake_segment.goto(position)
+        self.snake_segments.append(snake_segment)
+
+    def extend(self):
+        tail_position = self.snake_segments[-1].position()
+        self.add_segment(tail_position)
---
+++ day-20-21-snake-game/snake_game.py
@@ -0,0 +1,48 @@
+import time
+from turtle import Screen
+from food import Food
+from snake import Snake
+from score_board import ScoreBoard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("The Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score_board = ScoreBoard()
+
+screen.update()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+head = snake.snake_segments[0]
+
+while True:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if head.distance(food) < 20:
+        snake.extend()
+        score_board.increase_score()
+        food.generate_at_new_location()
+
+    x = head.xcor()
+    y = head.ycor()
+    if x > 280 or x < -280 or y > 280 or y < -280:
+        score_board.game_over()
+        break
+
+    for segment in snake.snake_segments[1:]:
+        if head.distance(segment) < 5:
+            score_board.game_over()
+            break
+
+screen.exitonclick()
